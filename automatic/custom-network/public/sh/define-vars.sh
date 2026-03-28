922074cbe6bff4a6c1362d640804ba251eeea3f0
Pramod NAIK <pramodvsnaik@gmail.com>
2026-03-28 13:03:07 +0100

    Fix echo command for SUBSCRIPTION_ID in define-vars.sh

--- automatic/custom-network/public/sh/define-vars.sh
+++ automatic/custom-network/public/sh/define-vars.sh
@@ -3,4 +3,4 @@ echo "VNET_NAME=automatic-vnet" >> $GITHUB_ENV
 echo "CLUSTER_NAME=automatic" >> $GITHUB_ENV
 echo "IDENTITY_NAME=automatic-uami" >> $GITHUB_ENV
 echo "LOCATION=eastus" >> $GITHUB_ENV
-echo "SUBSCRIPTION_ID=$(az account show --query id -o tsv)
+echo "SUBSCRIPTION_ID=$(az account show --query id -o tsv)" >> $GITHUB_ENV
