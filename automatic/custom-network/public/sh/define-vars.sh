91a80a214d9f15cea8705b552350782590fe1976
Pramod NAIK <pramodvsnaik@gmail.com>
2026-03-28 13:01:09 +0100

    Export environment variables to GITHUB_ENV
    Update variable definitions to export to GITHUB_ENV.

--- automatic/custom-network/public/sh/define-vars.sh
+++ automatic/custom-network/public/sh/define-vars.sh
@@ -1,6 +1,6 @@
-RG_NAME=automatic-rg
-VNET_NAME=automatic-vnet
-CLUSTER_NAME=automatic
-IDENTITY_NAME=automatic-uami
-LOCATION=eastus
-SUBSCRIPTION_ID=$(az account show --query id -o tsv)
+echo "RG_NAME=automatic-rg" >> $GITHUB_ENV
+echo "VNET_NAME=automatic-vnet" >> $GITHUB_ENV
+echo "CLUSTER_NAME=automatic" >> $GITHUB_ENV
+echo "IDENTITY_NAME=automatic-uami" >> $GITHUB_ENV
+echo "LOCATION=eastus" >> $GITHUB_ENV
+echo "SUBSCRIPTION_ID=$(az account show --query id -o tsv)
